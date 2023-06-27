1782acc6e321bb54684ce26128a10bf152375183
GamingDerp <46617199+GamingDerp@users.noreply.github.com>
2023-06-27 10:00:48 -0500

    Updated SNBMainFile.py
    Cleaned up comments

--- SNBMainFile.py
+++ SNBMainFile.py
@@ -5,11 +5,12 @@ from discord.utils import get
 from discord.ext import commands
 from datetime import datetime, timedelta
 
+# Bot Intents & Defining Bot
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
-bot.remove_command('help')
-###################################[ COG LOADING ]###################################
-# Loads Cogs
+bot.remove_command('help')    
+
+# Loading Cogs
 async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
