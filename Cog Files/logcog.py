1e3730dd09f4b8408080d9ff1b90045d1c98d0f6
GamingDerp <46617199+GamingDerp@users.noreply.github.com>
2023-07-28 09:38:54 -0500

    Update and rename logs.py to logcog.py

--- Cog Files/logcog.py
+++ Cog Files/logcog.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from datetime import datetime, timedelta
 
 # Logging Events Class
-class Logs(commands.Cog):
+class LogCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -228,4 +228,4 @@ class Logs(commands.Cog):
     
 
 async def setup(bot):
-    await bot.add_cog(Logs(bot))
+    await bot.add_cog(LogCog(bot))
