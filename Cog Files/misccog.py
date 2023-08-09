32a1797b825ccc505c56cba1458d347bc3018383
GamingDerp <46617199+GamingDerp@users.noreply.github.com>
2023-08-09 02:01:27 -0500

    Updated misccog.py
    Added tdadd, tddel, tdlist

--- Cog Files/misccog.py
+++ Cog Files/misccog.py
@@ -3,12 +3,29 @@ from discord.ext import commands
 from datetime import datetime, timedelta
 import time
 import asyncio
+import aiosqlite
         
 # Misc Commands Class
 class MiscCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+    
+    # Creating table on startup   
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.create_table()
+    
+    # Creates database table on startup
+    async def create_table(self):
+        async with aiosqlite.connect("dbs/todo.db") as db:
+            await db.execute('''
+                CREATE TABLE IF NOT EXISTS todos (
+                    user_id INTEGER, 
+                    todo TEXT
+                )
+            ''')
+            await db.commit()
+    
     # WhoIs Command
     @commands.command(aliases=["siohw", "Whois", "siohW", "WHOIS", "SIOHW"])
     async def whois(self, ctx, user:discord.Member):
@@ -72,6 +89,8 @@ class MiscCog(commands.Cog):
     
     @commands.Cog.listener()
     async def on_message_delete(self, message):
+        if message.author.bot:
+            return
         global sniped_message
         sniped_message = message
         
@@ -155,7 +174,8 @@ class MiscCog(commands.Cog):
         if converted_time == -2:
             await ctx.send("The time must be an integer!")
             return
-
+        
+        # Timer Embed
         e = discord.Embed(color=0xc700ff)
         e.description = "⏰ Started Reminder ⏰"
         e.add_field(name="Time", value=time)
@@ -163,24 +183,56 @@ class MiscCog(commands.Cog):
         e.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url)
         e.timestamp = datetime.utcnow()
         await ctx.send(embed=e)
-
-        channel = self.bot.get_channel(1119185446950408232)
-        e = discord.Embed(color=0xc700ff)
-        e.set_thumbnail(url=ctx.author.avatar.url)
-        e.set_author(name="⏰ User Set Reminder")
-        e.add_field(name="User", value=ctx.author.mention)
-        e.add_field(name="Time", value=time, inline=False)
-        e.add_field(name="Task", value=task, inline=False)
-        e.timestamp = datetime.utcnow()
-        await channel.send(embed=e)
- 
+        
+        # End Timer Embed
         await asyncio.sleep(converted_time)
         await ctx.send(ctx.author.mention)
         e = discord.Embed(color=0xc700ff)
         e.description = "⏰ Time's Up ⏰"
         e.add_field(name="Task", value=task)
         await ctx.send(embed=e)
-
         
+    # Todo Add Command
+    @commands.command(aliases=["ddadt", "Tdadd", "ddadT", "TDADD", "DDADT"])
+    async def tdadd(self, ctx, *, text):
+        author_id = ctx.author.id
+        async with aiosqlite.connect("dbs/todo.db") as db:
+            await db.execute("INSERT INTO todos (user_id, todo) VALUES (?, ?)", (author_id, text))
+            await db.commit()
+        await ctx.send(f"Added **{text}** to your todo list!")
+    
+    # ToDo Del Command
+    @commands.command(aliases=["leddt", "Tddel", "leddT", "TDDEL", "LEDDT"])
+    async def tddel(self, ctx, todo_num: int):
+        author_id = ctx.author.id
+        async with aiosqlite.connect("dbs/todo.db") as db:
+            cursor = await db.execute("SELECT rowid, todo FROM todos WHERE user_id = ?", (author_id,))
+            rows = await cursor.fetchall()
+            if todo_num <= 0 or todo_num > len(rows):
+                await ctx.send("Invalid todo number!")
+                return
+            todo_id, todo_text = rows[todo_num - 1]
+            await db.execute("DELETE FROM todos WHERE rowid = ?", (todo_id,))
+            await db.commit()
+        await ctx.send(f"Removed **{todo_text}** from your todo list!")
+    
+    # ToDo List Command
+    @commands.command(aliases=["tsildt", "Tdlist", "tsildT", "TDLIST", "TSILDT"])
+    async def tdlist(self, ctx, user: discord.User = None):
+        if user is None:
+            user = ctx.author
+        async with aiosqlite.connect("dbs/todo.db") as db:
+            cursor = await db.execute("SELECT todo FROM todos WHERE user_id = ?", (user.id,))
+            todos = await cursor.fetchall()
+        if not todos:
+            await ctx.send(f"**{user.name}** has no items in their todo list!")
+        else:
+            todo_list = "\n".join([f"• {todo[0]}" for todo in todos])
+            e = discord.Embed(color=0xc700ff)
+            e.set_author(name=f"{user.name}'s Todo List", icon_url=user.avatar.url)
+            e.description=todo_list
+        await ctx.send(embed=e)
+
+    
 async def setup(bot):
     await bot.add_cog(MiscCog(bot))
