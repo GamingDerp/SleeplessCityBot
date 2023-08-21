4df37d67fae10eb62cc3ce1901eafaa4418768cb
GamingDerp <46617199+GamingDerp@users.noreply.github.com>
2023-08-21 18:49:03 -0500

    Updated generalcog.py
    Added esteal, updated command count

--- Cog Files/generalcog.py
+++ Cog Files/generalcog.py
@@ -42,7 +42,7 @@ me.set_author(name="Bot Commands", icon_url="https://media.discordapp.net/attach
 me.set_thumbnail(url="https://media.discordapp.net/attachments/1065517294278676511/1078658592024043730/zZJfouNDCkPA.jpg")
 me.add_field(
     name="🧮 __Misc Commands__",
-    value=f"> `Whois`, `Avatar`, `Snipe`, `Deathhelp`, `/Pickle`, `Remind` \n> `Tdadd`, `Tddel`, `Tdlist`",
+    value=f"> `Whois`, `Avatar`, `Snipe`, `Deathhelp`, `/Pickle`, `Remind` \n> `Tdadd`, `Tddel`, `Tdlist`, `ESteal`",
 )
 
 # Staff Commands Embed
@@ -70,7 +70,7 @@ class Dropdown(discord.ui.Select):
             discord.SelectOption(label="General Commands",description="Help, Info, Test, Ping, Suggest +1 More", emoji="📌"),
             discord.SelectOption(label="Fun Commands", description="Coinflip, Ask, Reverse, Say, Lovetest +2 More", emoji="🎉"),
             discord.SelectOption(label="Action Commands", description="Sniff, Bite, Bonk, Vomit, Slap +16 More", emoji="🎯"),
-            discord.SelectOption(label="Misc Commands", description="Whois, Avatar, Snipe, Deathhelp, Pickle +4 More", emoji="🧮"),
+            discord.SelectOption(label="Misc Commands", description="Whois, Avatar, Snipe, Deathhelp, Pickle +5 More", emoji="🧮"),
             discord.SelectOption(label="Staff Commands", description="Purge, Ban, Unban, Kick, Timeout +3 More", emoji="🔰"),
             discord.SelectOption(label="Config Commands", description="SetPrefix, SetLog, SetStar, SetSuggest", emoji="⚙️"),
         ]
@@ -257,7 +257,7 @@ class GeneralCog(commands.Cog):
         )
         e.add_field(
             name="✧ __Statistics__",
-            value=f"> **Commands:** [55]"
+            value=f"> **Commands:** [56]"
 	          f"\n> **Code:** {total_lines} Lines"
                   f"\n> **Ping:** {round(self.bot.latency * 1000)}ms"
                   f"\n> **Users:** {true_member_count}"
