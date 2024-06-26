87e4430c75da61df9c3badb69c797c4e68c810ea
GamingDerp <46617199+GamingDerp@users.noreply.github.com>
2024-06-25 19:42:05 -0500

    Update actioncog.py

--- Cog Files/actioncog.py
+++ Cog Files/actioncog.py
@@ -14,7 +14,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Sniff another user")
     async def sniff(self, ctx, user:discord.Member):
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} sniffs {user.mention}!"
+        e.description = f"{ctx.author.mention} **sniffs** {user.mention}!"
         e.set_image(url="https://media.discordapp.net/attachments/807071768258805764/1131013212331069551/SniffingGif.gif"),
         await ctx.send(embed=e)
     
@@ -22,7 +22,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Bite another user")
     async def bite(self, ctx, user:discord.Member):
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} bites {user.mention}!"
+        e.description = f"{ctx.author.mention} **bites** {user.mention}!"
         bitegifs = [
             "https://media.discordapp.net/attachments/807071768258805764/1131664029052588122/AnimeBite3.gif", 
             "https://media.discordapp.net/attachments/807071768258805764/1131664029505556561/AnimeBite1.gif", 
@@ -35,7 +35,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Bonk another user")
     async def bonk(self, ctx, user:discord.Member):
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} bonks {user.mention}!"
+        e.description = f"{ctx.author.mention} **bonks** {user.mention}!"
         bonkgifs = [
             "https://media.discordapp.net/attachments/807071768258805764/1109240042238513233/BonkGif.gif", 
             "https://media.discordapp.net/attachments/807071768258805764/1124387823076769863/Bonk2Gif.gif"
@@ -47,7 +47,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Vomit on another user")
     async def vomit(self, ctx, user:discord.Member):
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} vomits on {user.mention}!"
+        e.description = f"{ctx.author.mention} **vomits on** {user.mention}!"
         vomitgifs = [
             "https://media.discordapp.net/attachments/807071768258805764/1131015043379626144/VomitGif.gif",
             "https://media.discordapp.net/attachments/807071768258805764/1135341052065234964/VomitGif2.gif"
@@ -59,7 +59,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Slap another user")
     async def slap(self, ctx, user:discord.Member):
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} slaps {user.mention}!"
+        e.description = f"{ctx.author.mention} **slaps** {user.mention}!"
         e.set_image(url="https://media.discordapp.net/attachments/807071768258805764/1106847432907685928/AnimeSlappingGif.gif"),
         await ctx.send(embed=e)
         
@@ -67,7 +67,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Punch another user")
     async def punch(self, ctx, user:discord.Member):
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} punches {user.mention}!"
+        e.description = f"{ctx.author.mention} **punches** {user.mention}!"
         e.set_image(url="https://media.discordapp.net/attachments/807071768258805764/1131018538577039390/PunchingGif.gif"),
         await ctx.send(embed=e)
 
@@ -75,7 +75,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Throw another user")
     async def throw(self, ctx, user:discord.Member):
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} throws {user.mention} off a cliff!"
+        e.description = f"{ctx.author.mention} **throws** {user.mention} **off a cliff!**"
         e.set_image(url="https://media.discordapp.net/attachments/807071768258805764/1116579751897878558/ThrowGif.gif"),
         await ctx.send(embed=e)
         
@@ -85,7 +85,7 @@ class ActionCog(commands.Cog):
         chance_response = ["they got caught", "they didn't get caught"]
         choice = random.choice(chance_response)
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} stalks {user.mention} and {choice}!"
+        e.description = f"{ctx.author.mention} **stalks** {user.mention} **and** {choice}!"
         if choice == "they got caught":
             e.set_image(url="https://cdn.discordapp.com/attachments/807071768258805764/1131011194325569616/CaughtStalkingGif.gif")
         else:
@@ -96,7 +96,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Kidnap another user")
     async def kidnap(self, ctx, user:discord.Member):
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} kidnaps {user.mention}!"
+        e.description = f"{ctx.author.mention} **kidnaps** {user.mention}!"
         e.set_image(url="https://media.discordapp.net/attachments/807071768258805764/1107074673361047673/AnimeKidnapGif.gif"),
         await ctx.send(embed=e)
     
@@ -104,7 +104,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Punt another user")
     async def punt(self, ctx, user:discord.Member):
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} punts {user.mention}!"
+        e.description = f"{ctx.author.mention} **punts** {user.mention}!"
         e.set_image(url="https://cdn.discordapp.com/attachments/807071768258805764/1123844694888165417/KickGif.gif"),
         await ctx.send(embed=e)
 
@@ -113,15 +113,13 @@ class ActionCog(commands.Cog):
     async def strangle(self, ctx, user:discord.Member):
         strangle_response = ["they crush their windpipes, killing them! RIP ☠️", f"causes them to faint!", f"they're hands slip, {user.mention} gets away!", f"they grab at {ctx.author.mention}, strangling them back!"]
         strangle_owner_response = ["it kills them! RIP ☠️", f"causes {user.mention} to faint!"]
-    
+        e = discord.Embed(color=0xc700ff)
         if ctx.message.author.id == owner_id:
-            e = discord.Embed(color=0xc700ff)
-            e.description = f"{ctx.author.mention} strangles {user.mention} and {random.choice(strangle_owner_response)}"
+            e.description = f"{ctx.author.mention} **strangles** {user.mention} **and** {random.choice(strangle_owner_response)}"
             e.set_image(url="https://cdn.discordapp.com/attachments/807071768258805764/1108639607370813440/AnimeStranglingGif.gif"),
             await ctx.send(embed=e)
         else:
-            e = discord.Embed(color=0xc700ff)
-            e.description = f"{ctx.author.mention} strangles {user.mention} and {random.choice(strangle_response)}"
+            e.description = f"{ctx.author.mention} **strangles** {user.mention} **and** {random.choice(strangle_response)}"
             e.set_image(url="https://cdn.discordapp.com/attachments/807071768258805764/1108639607370813440/AnimeStranglingGif.gif"),
             await ctx.send(embed=e)
         
@@ -130,15 +128,13 @@ class ActionCog(commands.Cog):
     async def stab(self, ctx, user:discord.Member):
         stab_response = ["it kills them! RIP ☠️", "they bleed out! RIP ☠️","they missed!", f"due to their momentum, the knife comes back and stabs {ctx.author.mention}!"]
         stab_owner_response = ["it kills them! RIP ☠️", f"{user.mention} bleeds out! RIP ☠️"]
-    
+        e = discord.Embed(color=0xc700ff)
         if ctx.message.author.id == owner_id:
-            e = discord.Embed(color=0xc700ff)
-            e.description = f"{ctx.author.mention} stabs {user.mention} and {random.choice(stab_owner_response)}"
+            e.description = f"{ctx.author.mention} **stabs** {user.mention} **and** {random.choice(stab_owner_response)}"
             e.set_image(url="https://media.discordapp.net/attachments/807071768258805764/1106846955612684308/AnimeStabbingGif.gif"),
             await ctx.send(embed=e)
         else:
-            e = discord.Embed(color=0xc700ff)
-            e.description = f"{ctx.author.mention} stabs {user.mention} and {random.choice(stab_response)}"
+            e.description = f"{ctx.author.mention} **stabs** {user.mention} **and** {random.choice(stab_response)}"
             e.set_image(url="https://media.discordapp.net/attachments/807071768258805764/1106846955612684308/AnimeStabbingGif.gif"),
             await ctx.send(embed=e)
     
@@ -147,15 +143,13 @@ class ActionCog(commands.Cog):
     async def shoot(self, ctx, user:discord.Member):
         shoot_response = ["it kills them! RIP ☠️", "it goes straight through their skull! RIP ☠️","it does nothing! They have a body of steel!", f"it ricochets off the wall and kills {ctx.author.mention}! RIP ☠️", "the gun jams!"]
         shoot_owner_response = ["it kills them! RIP ☠️", "it goes straight through their skull! RIP ☠️"]
-    
+        e = discord.Embed(color=0xc700ff)
         if ctx.message.author.id == owner_id:
-            e = discord.Embed(color=0xc700ff)
-            e.description = f"{ctx.author.mention} shoots at {user.mention} and {random.choice(shoot_owner_response)}"
+            e.description = f"{ctx.author.mention} **shoots at** {user.mention} **and** {random.choice(shoot_owner_response)}"
             e.set_image(url="https://cdn.discordapp.com/attachments/807071768258805764/1106843019321282591/AnimeShootingGif.gif"),
             await ctx.send(embed=e)
         else:
-            e = discord.Embed(color=0xc700ff)
-            e.description = f"{ctx.author.mention} shoots at {user.mention} and {random.choice(shoot_response)}"
+            e.description = f"{ctx.author.mention} **shoots at** {user.mention} **and** {random.choice(shoot_response)}"
             e.set_image(url="https://cdn.discordapp.com/attachments/807071768258805764/1106843019321282591/AnimeShootingGif.gif"),
             await ctx.send(embed=e)
             
@@ -183,7 +177,7 @@ class ActionCog(commands.Cog):
         if arg in deaths:
             death_description, image_url = deaths[arg]
             e = discord.Embed(color=0xc700ff)
-            e.description = f"{user.mention} {death_description} RIP ☠️"
+            e.description = f"{user.mention} {death_description} **RIP** ☠️"
             e.set_image(url=image_url)
             await ctx.send(embed=e)
         else:
@@ -193,7 +187,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Highfive another user")
     async def highfive(self, ctx, user:discord.Member): 
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} highfives {user.mention}!"
+        e.description = f"{ctx.author.mention} **highfives** {user.mention}!"
         e.set_image(url="https://media.discordapp.net/attachments/807071768258805764/1106851182279934072/AnimeHighfiveGif.gif"),
         await ctx.send(embed=e)
 
@@ -201,7 +195,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Poke another user")
     async def poke(self, ctx, user:discord.Member):
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} pokes {user.mention}!"
+        e.description = f"{ctx.author.mention} **pokes** {user.mention}!"
         e.set_image(url="https://media.discordapp.net/attachments/807071768258805764/1117137231950401617/PokeGif.gif"),
         await ctx.send(embed=e)
 
@@ -209,7 +203,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Pat another user")
     async def pat(self, ctx, user:discord.Member):
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} pats {user.mention}!"
+        e.description = f"{ctx.author.mention} **pats** {user.mention}!"
         e.set_image(url="https://cdn.discordapp.com/attachments/807071768258805764/1106851615320846386/AnimePatGif.gif"),
         await ctx.send(embed=e)
     
@@ -217,7 +211,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Lick another user")
     async def lick(self, ctx, user:discord.Member):
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} lick {user.mention}!"
+        e.description = f"{ctx.author.mention} **licks** {user.mention}!"
         lickgif = [
             "https://media.discordapp.net/attachments/807071768258805764/1131664016289300532/AnimeLick2.gif", 
             "https://media.discordapp.net/attachments/807071768258805764/1131664016868130978/AnimeLick3.gif", 
@@ -230,7 +224,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Hug another user")
     async def hug(self, ctx, user:discord.Member):  
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} hugs {user.mention}!"
+        e.description = f"{ctx.author.mention} **hugs** {user.mention}!"
         e.set_image(url="https://media.discordapp.net/attachments/807071768258805764/1106847914019536926/AnimeHuggingGif.gif"),
         await ctx.send(embed=e)
 
@@ -238,7 +232,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Kiss another user")
     async def kiss(self, ctx, user:discord.Member):
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} kisses {user.mention}!"
+        e.description = f"{ctx.author.mention} **kisses** {user.mention}!"
         e.set_image(url="https://media.discordapp.net/attachments/807071768258805764/1106848342966800404/AnimeKissingGif.gif"),
         await ctx.send(embed=e)
 
@@ -246,7 +240,7 @@ class ActionCog(commands.Cog):
     @commands.hybrid_command(description="Cuddle with another user")
     async def cuddle(self, ctx, user:discord.Member): 
         e = discord.Embed(color=0xc700ff)
-        e.description = f"{ctx.author.mention} cuddles with {user.mention}!"
+        e.description = f"{ctx.author.mention} **cuddles with** {user.mention}!"
         e.set_image(url="https://media.discordapp.net/attachments/807071768258805764/1106848675025666128/AnimeCuddlingGif.gif"),
         await ctx.send(embed=e)
 
